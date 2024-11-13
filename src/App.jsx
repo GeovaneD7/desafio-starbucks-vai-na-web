8e455b9055c0c27af4362b14a9c535a144aebcb7
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-11-13 17:23:59 -0300

    Finalizado o estilo do About

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Header from "./components/Header/Header";
 import News from "./components/News/News";
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
+import { GlobalStyle } from "./GlobalStyle";
 
 export default function App() {
   return (
     <>
     <BrowserRouter>
+      <GlobalStyle/>
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,18 @@
 import loja from "../../assets/loja.png";
+import { StyledAbout } from "./aboutStyle";
 
 export default function About() {
   return (
-    <main>
-      <h3>PREPARAÇÃO</h3>
-      <h2>Níveis de Torra</h2>
-      <p>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas sãos as torras que fazem parte dos níveis de torra Starbucks®</p>
-      <button>SAIBA MAIS</button>
-      <img src={loja} alt="" />
-    </main>
+    <StyledAbout>
+      <section>
+        <img src={loja} alt="" />
+        <div>
+          <h2>PREPARAÇÃO</h2>
+          <h3>Níveis de Torra</h3>
+          <p>Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas sãos as torras que fazem parte dos níveis de torra Starbucks®</p>
+          <input id="button" type="button" value="SAIBA MAIS" />
+        </div>
+      </section>
+    </StyledAbout>
   );
 };
