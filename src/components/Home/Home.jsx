577e915d9c02aa6ee31636ea541742541a3da180
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-11-14 16:20:13 -0300

    Adicionado os elementos da Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { StyledHome } from "./homeStyle";
-import copinhoV from "../../src/assets/vermelho_p.png";
-import copinhoA from "../../src/assets/amarelo_p.png";
-import copinhoL from "../../src/assets/laranja_p.png";
-import copoV from "../../src/assets/vermelho_g.png";
-import copoA from "../../src/assets/amarelo_g.png";
-import copoL from "../../src/assets/laranja_g.png";
+import vermelhoP from "../../assets/vermelho_p.png";
+import amareloP from "../../assets/amarelo_p.png";
+import laranjaP from "../../assets/laranja_p.png";
+import vermelhoG from "../../assets/vermelho_g.png";
+import amareloG from "../../assets/amarelo_g.png";
+import laranjaG from "../../assets/laranja_g.png";
 
 export default function Home() {
   const [copo, setcopo] = useState();
@@ -13,16 +13,27 @@ export default function Home() {
   return (
     <StyledHome>
       <section>
-        <h3>Mais que café</h3>
-        <h2>Isso é <h1>Starbucks</h1></h2>
-        <button>SAIBA MAIS</button>
-        <div>
+        <div id="enunciado">
+          <h3>Mais que café</h3>
+          <h2>Isso é <h1>Starbucks</h1></h2>
+          A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas quentes e frias, doces diferenciados e sanduíches.
+          <button>SAIBA MAIS</button>
+        </div>
+        <div id="circulo">
           <img src={copo} alt="" />
         </div>
       </section>
-      <section>
-        <button>{}</button>
-      </section>
+      <div id="amostras">
+        <button>
+          <img src={laranjaP} alt="" />
+        </button>
+        <button>
+          <img src={vermelhoP} alt="" />
+        </button>
+        <button>
+          <img src={amareloP} alt="" />
+        </button>
+      </div>
     </StyledHome>
   );
 };
